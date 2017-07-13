0ae5b9aaf43166fc2450727877c8a519925480bc
Volodya Martynson <vladimir.armaseti@yandex.ru>
2017-07-13 16:54:47 +0700

    New design of letter "galeznikov"

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var nodemailer = require('nodemailer');
 const fs = require('fs');
 
 let transporter = nodemailer.createTransport({
-    host: 'smtp.prodazha-optom.ru',
+    host: 'smtp2.gidroteh-expert.ru',
     port: 465,
     pool: true,
     secure: true, // use TLS
@@ -16,31 +16,31 @@ let transporter = nodemailer.createTransport({
     }
 });
 
-let email = 'zogacc@gmail.com';
+let email = 'web-6roel@mail-tester.com';
 let htmlPath = 'dist/index.html';
-let txtPath = 'plain-text/flancy.txt';
+let txtPath = 'plain-text/galeznik.txt';
 
 
 let mailOptions = {
-    from: '"ТД Армасети" <sale@prodazha-optom.ru>', // sender address
+    from: '"ТД Армасети" <sale@gidtoteh-expert.ru>', // sender address
     to: email, // list of receivers
     subject: 'subject #4',
     headers: {
-        "List-Unsubscribe": `<http://prodazha-optom.ru/unsubscribe/447447574654/58675865/7tftf65d7rd75d54d>`,
+        "List-Unsubscribe": `<http://gidroteh-expert.ru/unsubscribe/447447574654/58675865/7tftf65d7rd75d54d>`,
         "list-id" : `test sending`,
     },
     envelope : {
-        from : '"Bounce" <abuse@prodazha-optom.ru>',
+        from : '"Bounce" <abuse@gidroteh-expert.ru>',
         to : email
     },
-    replyTo : '"ТД Армасети" <sale@prodazha-optom.ru>',
+    replyTo : '"ТД Армасети" <sale@gidroteh-expert.ru>',
     text: template(fs.readFileSync(txtPath, 'utf8'), {
         email: email,
-        unsub: `http://prodazha-optom.ru/unsubscribe/447447574654/58675865`
+        unsub: `http://gidroteh-expert.ru/unsubscribe/447447574654/58675865`
     }),// Subject line {email: item.email}
     html: template(fs.readFileSync(htmlPath, 'utf8'), {
         email: email,
-        unsub: `http://prodazha-optom.ru/unsubscribe/447447574654/58675865`
+        unsub: `http://gidroteh-expert.ru/unsubscribe/447447574654/58675865`
     }) // html body
 };
 transporter.sendMail(mailOptions, function (error, info) {
